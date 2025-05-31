9f785e1ec3235ab8bfbf963745c32f5dc798d693
Agustin Exequiel Gimenez <exequielgimenez026@gmail.com>
2025-05-30 22:21:37 -0300

    Comandos basicos de Git

---
+++ codigoExequielGimenez.py
@@ -0,0 +1,18 @@
+# 🔧 Configuración inicial
+
+# git config --global user.name "Seu Nome"
+# git config --global user.email "seu@email.com"
+
+# 📁 Creación e inicialización
+# git init
+
+# 📥 Clonar un repositorio
+# git clone https://github.com/usuario/repositorio.git
+
+# 📄 Control de estado y cambios
+# git status
+# git add .
+# git commit -m "Mensagem do commit"
+
+# Actualiza su repositorio local con los cambios del remoto.
+# git push
